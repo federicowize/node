64460db8c3bfcd1594118069c434f9ffb2c84e45
federico <federico@wize-net.co.jp>
2020-03-23 09:27:10 +0900

    add type

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -4,9 +4,7 @@ import { Router } from "express";
 export default module.exports = () =>
   sync("**/*.js", { cwd: `${__dirname}/` })
     .map((filename: string) => require(`./${filename}`))
-
     .filter((router: Router) => Object.getPrototypeOf(router) == Router)
-
     .reduce(
       (AcumulatorRouter: Router, nextRouter: Router) =>
         AcumulatorRouter.use(nextRouter),
--- server/src/routes/users/getUsers.ts
+++ server/src/routes/users/getUsers.ts
@@ -2,7 +2,7 @@ import  * as express from "express";
 
 module.exports = express
   .Router({ mergeParams: true })
-  .get("/users", async (req: any, res: any, next: any) => {
+  .get("/users", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.log("enter here", req.originalUrl);
     res.send("Admin Homepage");
   });
